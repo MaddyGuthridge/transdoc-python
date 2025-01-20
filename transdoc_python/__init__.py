438314e953272f7f641cbfccfa563413a027b51e
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-20 23:04:46 +1100

    Implement transdoc-python

--- transdoc_python/__init__.py
+++ transdoc_python/__init__.py
@@ -1,8 +1,40 @@
 """
 # Transdoc Python
 
-A Transdoc handler for Python docstrings.
+A Transdoc handler for Python docstrings, using libcst to rewrite
+documentation.
 """
 
+from typing import IO
+import libcst as cst
+from libcst import MetadataWrapper
+from transdoc import TransdocHandler, TransdocTransformer
+
+from transdoc_python.__visitor import DocstringVisitor
+
+
 class TransdocPythonHandler:
-    ...
+    """
+    A Transdoc handler for Python docstrings.
+    """
+
+    def get_file_matchers(self):
+        return ["py", "pyi"]
+
+    def transform_file(
+        self,
+        transformer: TransdocTransformer,
+        in_path: str,
+        in_file: IO,
+        out_file: IO | None,
+    ) -> None:
+        parsed = MetadataWrapper(cst.parse_module(in_file.read()))
+        visitor = DocstringVisitor(transformer, in_path)
+        updated_cst = parsed.visit(visitor)
+        visitor.raise_errors()
+        if out_file is not None:
+            out_file.write(updated_cst.code)
+
+
+if __name__ == "__main__":
+    handler: TransdocHandler = TransdocPythonHandler()
