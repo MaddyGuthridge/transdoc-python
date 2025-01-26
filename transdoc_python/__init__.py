6588fd38d040fb326549e5376deda2976225723e
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-26 17:24:31 +1100

    Handle errors when libcst fails

--- transdoc_python/__init__.py
+++ transdoc_python/__init__.py
@@ -6,16 +6,21 @@ documentation.
 """
 
 from importlib.metadata import version
+from logging import getLogger
 from pathlib import Path
 from typing import IO
 import libcst as cst
 from libcst import MetadataWrapper
+import libcst
 from transdoc import TransdocHandler, TransdocTransformer
 
 from transdoc_python.__visitor import DocstringVisitor
 
 
 __version__ = version("transdoc-python")
+
+
+log = getLogger("transdoc-python")
 
 
 class TransdocPythonHandler:
@@ -33,12 +38,22 @@ class TransdocPythonHandler:
         in_file: IO,
         out_file: IO | None,
     ) -> None:
-        parsed = MetadataWrapper(cst.parse_module(in_file.read()))
-        visitor = DocstringVisitor(transformer, in_path)
-        updated_cst = parsed.visit(visitor)
-        visitor.raise_errors()
-        if out_file is not None:
-            out_file.write(updated_cst.code)
+        try:
+            parsed = MetadataWrapper(cst.parse_module(in_file.read()))
+            visitor = DocstringVisitor(transformer, in_path)
+            updated_cst = parsed.visit(visitor)
+            visitor.raise_errors()
+            if out_file is not None:
+                out_file.write(updated_cst.code)
+        except libcst.ParserSyntaxError:
+            # Error while parsing the file
+            # Just copy the file instead
+            # TODO: More-integrated way to warn of this
+            log.warning(
+                f"{in_path} failed to parse using libcst. Copying file as-is instead."
+            )
+            if out_file is not None:
+                out_file.write(in_file.read())
 
 
 __all__ = [
